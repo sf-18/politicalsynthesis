636b160764952075f19d712e5923b0d6e80ffcb4
unknown <ekch@bu.edu>
2019-09-14 14:35:56 -0400

    helloeric

--- politicalsynthesis/polidata/models.py
+++ politicalsynthesis/polidata/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Candidate(models.Model):
+    candidate_photo = models.CharField
+    candidate_name = models.CharField
+    candidate_party = models.CharField
