71ffd2f45b54bde33429357c8fc2bb1ff05e3eae
Eric <ehchao88@gmail.com>
2019-09-14 15:07:43 -0400

    Candidate models

--- politicalsynthesis/polidata/models.py
+++ politicalsynthesis/polidata/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Candidate(models.Model):
-    candidate_photo = models.CharField
-    candidate_name = models.CharField
-    candidate_party = models.CharField
+    candidate_photo = models.TextField()
+    candidate_name = models.CharField(max_length=100)
+    candidate_party = models.CharField(max_length=50)
+    candidate_incumbency = models.CharField(max_length=100)
