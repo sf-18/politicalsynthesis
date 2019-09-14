12b33001117d1fb5298c0e8425fd5837ad0b180a
unknown <ekch@bu.edu>
2019-09-14 14:54:42 -0400

    putting together views pages

--- politicalsynthesis/polidata/urls.py
+++ politicalsynthesis/polidata/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('bylocation', views.location_selection, name='location_selection'),
+    path('candidate_list', views.candidate_list, name='candidate_list')
 ]
