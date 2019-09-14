922908c3de858c0d49dcfcb9d874351e031ef399
unknown <ekch@bu.edu>
2019-09-14 15:41:41 -0400

    with templates folder

--- politicalsynthesis/polidata/views.py
+++ politicalsynthesis/polidata/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.template import loader
 
 def index(request):
-    return HttpResponse("landing page")
+    template = loader.get_template('polidata/index.html')
+    context = {"state" : "state_name"}
+    return HttpResponse(template.render(context, request))
 
 def location_selection(request):
     return HttpResponse("input zip and state")
