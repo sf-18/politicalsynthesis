21a16f5f95ac97f1e61304525cf6980b9f06ed00
sf-18 <hdays112397@gmail.com>
2019-09-14 12:00:54 -0400

    initial setup.py file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='politicalsynthesis',
+      version='0.0',
+      description='Automated synthesis and organization of political content from current elections to increase political literacy and voter turnout.',
+      author='Sarah Feng, Anika Cheerla',
+      packages=['search_google']
+     )
