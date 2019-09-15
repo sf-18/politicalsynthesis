e645dee5e1474be98502f2f0ab1f449805c19205
sf-18 <hdays112397@gmail.com>
2019-09-15 04:56:49 -0400

    Check setup

--- summarize.py
+++ summarize.py
@@ -1,19 +1,17 @@
-"""
-Summary should be a list of the abstracted summaries corresponding to the topic at keyword at thesame index.
-""" 
-import json
-summary_file_path = "/summaries/"
-from prepro import data_builder
-import torch
+from src.presumm.src.prepro import data_builder
 from pytorch_transformers import BertTokenizer
+from src.presumm.models import data_loader, model_builder
+from src.presumm.models.data_loader import load_dataset
+from src.presumm.models.loss import abs_loss
+from src.presumm.models.model_builder import AbsSummarizer
+from src.presumm.models.predictor import build_predictor
+from src.presumm.models.trainer import build_trainer
 
-import distributed
-from PreSumm.models import data_loader, model_builder
-from PreSumm.models.data_loader import load_dataset
-from PreSumm.models.loss import abs_loss
-from PreSumm.models.model_builder import AbsSummarizer
-from PreSumm.models.predictor import build_predictor
-from PreSumm.models.trainer import build_trainer
+import json
+import src.presumm.src.distributed
+import torch
+
+summary_file_path = "/summaries/"
 
 def preprocess(text):
 	data_builder.tokenize(text)
@@ -21,20 +19,18 @@ def preprocess(text):
 	data_builder.format_to_bert(text)
 	return text
 
-checkpoint = ""
+checkpoint = "model/model_step_148000.pt"
 
 def summarize(text):
 	model = AbsSummarizer(args, device, checkpoint)
-    model.eval()
+	model.eval()
 
-    test_iter = data_loader.Dataloader(args, load_dataset(args, 'test', shuffle=False),
-                                       args.test_batch_size, device,
-                                       shuffle=False, is_test=True)
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True, cache_dir=args.temp_dir)
-    symbols = {'BOS': tokenizer.vocab['[unused0]'], 'EOS': tokenizer.vocab['[unused1]'],
-               'PAD': tokenizer.vocab['[PAD]'], 'EOQ': tokenizer.vocab['[unused2]']}
-    predictor = build_predictor(args, tokenizer, symbols, model, logger)
-    predictor.translate(test_iter, step)
+	test_iter = data_loader.Dataloader(preprocess(text), 500, 'cpu', shuffle=False, is_test=True)
+	tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=False)
+	symbols = {'BOS': tokenizer.vocab['[unused0]'], 'EOS': tokenizer.vocab['[unused1]'],
+	           'PAD': tokenizer.vocab['[PAD]'], 'EOQ': tokenizer.vocab['[unused2]']}
+	predictor = build_predictor(tokenizer, symbols)
+	predictor.translate(test_iter, step)
 
 def write_to_json(keywords, summary, candidate_name):
 	data = {}
