5a95461d190b86dc99be80ef7f447fdbf3761d67
Eric <ehchao88@gmail.com>
2019-09-15 04:07:14 -0400

    Site functionality

--- politicalsynthesis/polidata/urls.py
+++ politicalsynthesis/polidata/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('elections', views.elections, name='elections'),
     path('candidate_list', views.candidate_list, name='candidate_list'),
-    path('candidate_page', views.candidate_page, name='candidate_page')
+    path('<candidate_name>', views.candidate_page, name='candidate_page')
 ]
